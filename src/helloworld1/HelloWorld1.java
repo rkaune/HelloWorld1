d896097587698da4a7cd2ae749318df18a81551c
rkaune <rkaune@ugcloud.ca>
2021-03-15 13:31:05 -0400

    Update HelloWorld1.java
    deleted one SOP

--- src/helloworld1/HelloWorld1.java
+++ src/helloworld1/HelloWorld1.java
@@ -17,7 +17,7 @@ public class HelloWorld1 {
     public static void main(String[] args) {
         System.out.println(" Alicia Keys ");
         System.out.println(" Alicia Keys ");
-        System.out.println(" Alicia Keys ");
+
         
     }
     
